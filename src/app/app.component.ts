520574803e97c27c83701256e437fbd0a34ad0c8
Carlos Gomez <gemec98@gmail.com>
2020-07-20 21:17:30 -0500

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pipes';
+
+  nombre = 'Wolverine';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  porcentaje= 0.3245;
+
+  salario= 1864000;
+
+  heroe={
+    nombre:'Logan',
+    clave:'Wolverine',
+    edad:500,
+    direccion:{
+      calle: 'primera',
+      casa:2
+    }
+  }
+
+  valorPromesa= new Promise<string>((resolve)  => {
+
+    setTimeout(() =>{
+      resolve('Llego la data');
+    }, 3500);
+  });
+
+  fecha= new Date();
+
 }
